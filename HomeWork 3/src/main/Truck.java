f6574508a05afacc28524c6ba6712a9d2534db62
RRR3d <105751665+RRR3d@users.noreply.github.com>
2022-05-18 01:08:08 +0300

    kk

--- HomeWork 3/src/main/Truck.java
+++ HomeWork 3/src/main/Truck.java
@@ -2,4 +2,32 @@ package main;
 
 public class Truck extends Vechicle{
     private double load_Capacity;
+    private float towing_Capacity;
+
+    public Truck(String m_name, int cylinder, String ownerName, double load_Capacity, float towing_Capacity) {
+        super(m_name, cylinder, ownerName);
+        this.load_Capacity = load_Capacity;
+        this.towing_Capacity = towing_Capacity;
+    }
+
+    public Truck(double load_Capacity, float towing_Capacity) {
+        this.load_Capacity = load_Capacity;
+        this.towing_Capacity = towing_Capacity;
+    }
+
+    public double getLoad_Capacity() {
+        return load_Capacity;
+    }
+
+    public void setLoad_Capacity(double load_Capacity) {
+        this.load_Capacity = load_Capacity;
+    }
+
+    public float getTowing_Capacity() {
+        return towing_Capacity;
+    }
+
+    public void setTowing_Capacity(float towing_Capacity) {
+        this.towing_Capacity = towing_Capacity;
+    }
 }
