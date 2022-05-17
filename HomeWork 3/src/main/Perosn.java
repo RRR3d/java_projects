22e9d3521fcbfd04d2526fa7151e1c157efb7847
RRR3d <105751665+RRR3d@users.noreply.github.com>
2022-05-18 01:16:22 +0300

    d

---
+++ HomeWork 3/src/main/Perosn.java
@@ -0,0 +1,24 @@
+package main;
+
+public class Person {
+    private String name;
+
+    public Person() {
+    }
+    public Person(String theName) {
+        this.name = theName;
+    }
+    public Person(Person theObject)
+    {
+
+    }
+    public String getName()
+    {...}
+    public void setName(String theName)
+    {...}
+    public String toString()
+    {...}
+    public boolean equals(Object other)
+    {...}
+}
+
